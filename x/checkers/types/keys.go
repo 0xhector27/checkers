5f06204cd597da8735ad91d68d15f0e23609020d
smartcoding51 <keinakano415@gmail.com>
2022-09-17 01:15:30 +0900

    endblock - auto expiring games

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -56,6 +56,15 @@ const (
 )
 
 const (
-	MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
+	// MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
+	// Temporarily set to 5 mins
+	MaxTurnDuration = time.Duration(5 * 60 * 1000_000_000) // 5 minutes
 	DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
+)
+
+const (
+	GameForfeitedEventType      = "game-forfeited"
+	GameForfeitedEventGameIndex = "game-index"
+	GameForfeitedEventWinner    = "winner"
+	GameForfeitedEventBoard     = "board"
 )
